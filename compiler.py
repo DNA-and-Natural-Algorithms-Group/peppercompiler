d63a020b3869e37dbc20183bd785f8a40233411b
sligocki <unknown>
2007-07-18 21:08:29 +0000

    Broke compiling into 2 different functions to accomidate the NUPACK delay.

--- compiler.py
+++ compiler.py
@@ -1,4 +1,4 @@
-import sys, pickle
+import sys, pickle, re
 from circuit_parser import load_circuit
 from kinetics import read_nupack, test_kinetics
 
@@ -9,9 +9,11 @@ def compiler(infilename):
   ### TODO-maybe: deal with false input issue, where output c1 and the 4 inputs c1 are all treated independent
   circuit.output_nupack(infilename+".des")
   # Call Zadeh's Design software
-  save(circuit, infilename+".save1")
+  save(circuit, infilename+".save")
   ### TODO: Work out automatic way of doing this with Joe Zadeh.
-  raw_input("Run %s.des in NUPACK, save the result in %s.summary and press enter to continue." % (infilename, infilename))
+  #raw_input("Run %s.des in NUPACK, save the result in %s.summary and press enter to continue." % (infilename, infilename))
+def finish(infilename):
+  circuit = load(infilename+".save")
   # Read results
   seqs, mfe_structs = read_nupack(infilename+".summary")
   # Prepare for Schaffer's Multistrand
@@ -24,11 +26,22 @@ def compiler(infilename):
   # TODO: Inform user of results
 
 def save(obj, filename):
-  """Save an object in case of crash or rerun."""
+  """Save an object for later finishing."""
   f = file(filename, "w")
   pickle.dump(obj, f)
   f.close()
+def load(filename):
+  """Load it back."""
+  f = file(filename, "r")
+  obj = pickle.load(f)
+  f.close()
+  return obj
 
 if __name__ == "__main__":
-  compiler(sys.argv[1])
+  filename = sys.argv[1]
+  p = re.match(r"(.*)\.save", filename)
+  if p:
+    finish(p.group(1)) # Finish started process
+  else:
+    compiler(filename) # Start process
 
