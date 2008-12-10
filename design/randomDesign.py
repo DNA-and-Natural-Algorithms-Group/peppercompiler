88a2ecc1125cc1161577e391b65e1dbeb8648aa0
sligocki <unknown>
2008-12-10 19:47:13 +0000

    Cleaned up and commented some.

--- design/randomDesign.py
+++ design/randomDesign.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python
-"""Designs a sequence randomly."""
+"""
+Designs sequences randomly constrained to the forced WC-complimentarity by basepairing.
+Uses Joe Zadah's input and output formats to work with compiler.
+"""
 from __future__ import division
 
-import sys, random, string, re
+import random
+import string
 
 # Extend path to see compiler library and the HU2dotParen library
+import sys
 here = sys.path[0] # System path to this module.
 sys.path += (here+"/..", here+"/../HU2dotParen")
+
 from DNAfold import DNAfold
 from HU2dotParen import HU2dotParen
 from nupack_in_parser import load_design
 from DNA_classes import group, compliment
 
 def random_choice(group):
+  """Randomly chooses an element (and gives sensical error if group has size 0)."""
   assert len(group) > 0
   return random.choice(group)
 
 class default_list(list):
+  """A list which defaults items (using a default function)."""
   def __init__(self, default_func):
     self.default = default_func
   def __getitem__(self, index):
+    """Get foo[index]. If it's not in list, extend with defaults till it is."""
     if len(self) <= index:
       self.extend([self.default(a) for a in range(len(self), index+1)])
     return list.__getitem__(self, index)
 
 class cool_set(set):
+  """A set which also has some a few special attributes."""
   def __init__(self, *args):
     self.base = None
     self.const = None
     set.__init__(self, *args)
 
-def set_str(string, i, val):
+def str_replace(string, i, val):
+  """Replace string[i] with val and return new string."""
   assert len(val) == 1
   return string[:i] + val + string[i+1:]
 
@@ -55,7 +66,8 @@ class Connect(object):
       self.data[i][j][False] = data_x[True]
 
 def design(infilename, outfile):
-  global d # DEBUG
+  """Design sequences."""
+  global d # For debugging purposes
   d = load_design(infilename)
   
   # Create complementarity matrix
@@ -79,7 +91,7 @@ def design(infilename, outfile):
         data[True].base = random_choice(data[True].const)
         data[False].base = compliment[ data[True].base ]
       
-      seq.seq = set_str(seq.seq, j, data[True].base)
+      seq.seq = str_replace(seq.seq, j, data[True].base)
 
   # Test thermo and redesign to improve structure
   build_structs(d.complexes)
@@ -96,7 +108,7 @@ def design(infilename, outfile):
     # Propogate mutations
     for i, seq in enumerate(d.seqs.values()):
       for j, symb in enumerate(seq.seq):
-        seq.seq = set_str(seq.seq, j, connect.data[i][j][True].base)
+        seq.seq = str_replace(seq.seq, j, connect.data[i][j][True].base)
     new_res = score(d.complexes)
     print len(new_res), len(res), len(best_res)
     res = new_res
@@ -132,6 +144,7 @@ def build_structs(complexes):
     assert l == 0 and end_iter(lengths)
 
 def end_iter(foo):
+  """Iterate to the end of foo."""
   try:
     foo.next()
     return False
@@ -139,6 +152,7 @@ def end_iter(foo):
     return True
 
 def build_seq(compl):
+  """Produce flat sequence for a structure/complex by expanding out the strands and subsequences/subdomains."""
   return string.join([ string.join([seq.get_seq() for seq in strand], "") for strand in compl.strands], "+")
 
 def score(complexes):
@@ -152,7 +166,8 @@ def score(complexes):
   return diffs
 
 def diff(lable, str_a, str_b):
-  ### TODO-maybe: make correct by having:
+  """Return bp differences between 2 secondary structures."""
+  ### TODO: make correct by having:
   ###       ..(((...+...)))..  to  ..(((..)+(..)))..  be 4 errors not just 2
   #print lable.name, str_a, str_b
   assert len(str_a) == len(str_b)
@@ -161,14 +176,15 @@ def diff(lable, str_a, str_b):
   return [(lable, i) for i, (a, b) in enumerate(zip(str_a, str_b)) if a != b]
 
 def output_sequences(d, connect, fn):
+  """Output sequences in Joe's format."""
   f = file(fn, "w")
   for compl in d.complexes.values():
-    #print compl
+    # Write structure/complex (with dummy content)
     f.write("%d:%s\n" % (0, compl.name))
-    gc_content = (compl.seq.count("C") + compl.seq.count("G")) / (len(compl.seq) - len(compl.strands))
+    gc_content = (compl.seq.count("C") + compl.seq.count("G")) / (len(compl.seq) - len(compl.strands)) # - len(compl.strands) because of the +'s in the compl.seq
     f.write("%s %f %f %d\n" % (compl.seq, 0, gc_content, 0))
-    f.write("%s\n" % compl.dp_struct)
-    f.write("%s\n" % compl.mfe_struct)
+    f.write("%s\n" % compl.dp_struct)   # Target structure
+    f.write("%s\n" % compl.mfe_struct)  # MFE structure of chosen sequences
   for seq in d.seqs.values():
     # Write sequence (with dummy content)
     f.write("%d:%s\n" % (0, seq.name))
@@ -185,7 +201,8 @@ def output_sequences(d, connect, fn):
   f.close()
 
 if __name__ == "__main__":
+  import re
   in_name = sys.argv[1]
-  out_name = re.sub(r"\.des\Z", "", in_name) + ".mfe"
+  out_name = re.sub(r"\.des\Z", r"\.mfe", in_name) # Makes foo.des => foo.mfe
   design(in_name, out_name)
 
