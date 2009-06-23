f8319ab25e8ac62ae0d81144380c40821a7ac189
sligocki <unknown>
2009-06-23 18:37:02 +0000

    Got rid of debugging messages.

--- DNAfold.py
+++ DNAfold.py
@@ -12,9 +12,7 @@ def DNAfold(seq, temp=25):
   """Run the installed thermodynamic mfe package"""
   if config.thermo == config.NUPACK:
     import DNAfold_Nupack
-    print "Nupack", config.nupack_path
     return DNAfold_Nupack.DNAfold(seq, temp, exe=config.nupack_path)
   else:
     import DNAfold_Vienna
-    print "Vienna", config.vienna_path, config.par_file
     return DNAfold_Vienna.DNAfold(seq, temp, exe=config.vienna_path, par_file=config.par_file)
