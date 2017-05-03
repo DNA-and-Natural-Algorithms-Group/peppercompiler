f7216b4f89a9472dc4892fae27ad51ccb1dd39e7
Constantine Evans <cge@dna.caltech.edu>
2017-05-02 20:39:31 -0700

    add spuriousSSM, build functions in setup.py, and a wrapper

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from distutils.command.build import build
+from setuptools.command.develop import develop
+
+class build_with_spurious(build):
+    def run(self):
+        import os
+        print "Compiling spuriousSSM"
+        os.system("cc -Wall -O3 SpuriousDesign/spuriousSSM.c -o pepper/_spuriousSSM -lm")
+        
+        build.run(self)
+
+class develop_with_spurious(develop):
+    def run(self):
+        import os
+        print "Compiling spuriousSSM"
+        os.system("cc -Wall -O3 SpuriousDesign/spuriousSSM.c -o pepper/_spuriousSSM -lm")
+        
+        develop.run(self)
 
 setup(
     name = "pepper",
@@ -10,12 +28,16 @@ setup(
     install_requires = ["pyparsing","xdg","six"],
 
     include_package_data=True,
-
+    package_data={'pepper': ['_spuriousSSM']},
+    
+    cmdclass={'build': build_with_spurious, 'develop': develop_with_spurious},
+    
     entry_points={ 'console_scripts': [
         'pepper-compiler = pepper.compiler:main',
         'pepper-design-spurious = pepper.design.spurious_design:main',
         'pepper-finish = pepper.finish:main',
-        'pepper-config = pepper.config:main']},
+        'pepper-config = pepper.config:main',
+        'spuriousSSM = pepper._spuriousSSM_wrapper:main']},
 
     author = "Constantine Evans et al (this version)",
     author_email = "cgevans@evans.foundation",
