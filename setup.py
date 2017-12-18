0f611828f95e0d1d1cf9f3723a4fe10baf5caf43
Constantine Evans <cevans@evanslabs.org>
2017-12-18 01:13:42 -0800

    bump version, reformat setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from distutils.command.build import build
 from setuptools.command.develop import develop
+
 
 class build_with_spurious(build):
     def run(self):
@@ -11,38 +12,43 @@ class build_with_spurious(build):
             cc = os.environ['CC']
         else:
             cc = "cc"
-        os.system("{} -Wall -O3 peppercompiler/SpuriousDesign/spuriousSSM.c -o peppercompiler/_spuriousSSM -lm".format(cc))
+        os.system(
+            "{} -Wall -O3 peppercompiler/SpuriousDesign/spuriousSSM.c -o peppercompiler/_spuriousSSM -lm".
+            format(cc))
 
         build.run(self)
+
 
 class develop_with_spurious(develop):
     def run(self):
         import os
-        os.system("cc -Wall -O3 peppercompiler/SpuriousDesign/spuriousSSM.c -o peppercompiler/_spuriousSSM -lm")
+        os.system(
+            "cc -Wall -O3 peppercompiler/SpuriousDesign/spuriousSSM.c -o peppercompiler/_spuriousSSM -lm"
+        )
 
         develop.run(self)
 
+
 setup(
-    name = "peppercompiler",
-    version = "0.1.0",
-    packages = ['peppercompiler', 'peppercompiler.design'],
-
-    install_requires = ["pyparsing","six"],
-
+    name="peppercompiler",
+    version="0.1.2",
+    packages=['peppercompiler', 'peppercompiler.design'],
+    install_requires=["pyparsing", "six"],
     include_package_data=True,
-    package_data={'peppercompiler': ['_spuriousSSM', 'SpuriousDesign/spuriousSSM.c']},
-
+    package_data={
+        'peppercompiler': ['_spuriousSSM', 'SpuriousDesign/spuriousSSM.c']
+    },
     test_suite='peppercompiler.tests',
-
-    cmdclass={'build': build_with_spurious, 'develop': develop_with_spurious},
-
-    entry_points={ 'console_scripts': [
-        'pepper-compiler = peppercompiler.compiler:main',
-        'pepper-design-spurious = peppercompiler.design.spurious_design:main',
-        'pepper-finish = peppercompiler.finish:main',
-        'spuriousSSM = peppercompiler._spuriousSSM_wrapper:main']},
-
-    author = "Constantine Evans et al (this version)",
-    author_email = "cge@dna.caltech.edu",
-    description = "PepperCompiler in a pythonic form"
-)
+    cmdclass={'build': build_with_spurious,
+              'develop': develop_with_spurious},
+    entry_points={
+        'console_scripts': [
+            'pepper-compiler = peppercompiler.compiler:main',
+            'pepper-design-spurious = peppercompiler.design.spurious_design:main',
+            'pepper-finish = peppercompiler.finish:main',
+            'spuriousSSM = peppercompiler._spuriousSSM_wrapper:main'
+        ]
+    },
+    author="Constantine Evans et al (this version)",
+    author_email="cge@dna.caltech.edu",
+    description="PepperCompiler in a pythonic form")
