b61a5dffa9abf7dc260451e4bba01a5031a34589
Constantine Evans <const@costi.net>
2023-12-13 19:31:49 +0000

    use setuptools, not distutils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from distutils.command.build import build
+from setuptools.command.build import build
 from setuptools.command.develop import develop
 
 
